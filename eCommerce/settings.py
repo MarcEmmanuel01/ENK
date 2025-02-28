5c582e7d7aee4a1ae85d44a553a6fee4afd15149
MarcEmmanuel01 <emmanuelkassi07@gmail.com>
2025-02-28 21:24:14 +0000

    28 (4)  fevrier 2025

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2at5zucn00%$4%aj)d@tx!bd2c%b6-qzg1@em3zvfhheq=k3qy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.1.67']
 
 # change the default user models to our custom model
 AUTH_USER_MODEL = 'accounts.User'
